2313890a91a5e82a8bced124733489999946c19c
Divyesh Ramani <divyesh.ramani@disney.com>
2019-04-21 17:33:26 -0700

    bug fix

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -2,7 +2,7 @@ data "aws_availability_zones" "available" {}
 variable "env" {}
 variable "aws_region" {}
 variable "cidr" {}
-variable "internal-ips" {}
+variable "internal_ips" {}
 variable "cidrs" {
   type = "map"
 }
